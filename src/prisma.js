122ec011648729b5ad5dd76aa34c5973ab524dc3
sagar rathna sabapathy <sagar.rathnasabapathy@diconium.com>
2018-10-27 11:42:45 +0200

    prisma createPost mutation

--- src/prisma.js
+++ src/prisma.js
@@ -14,3 +14,24 @@ prisma.query.users(null, '{id, name, email}').then((data) => {
 prisma.query.comments(null, '{id text author {id name}}').then((data) => {
     console.log(JSON.stringify(data, undefined, 2));
 })
+
+//createPost mutation
+prisma.mutation.createPost({
+    data: {
+        title: 'prisma is awesome',
+        body: 'bla bla',
+        published: true,
+        author: {
+            connect: {
+                id: 'cjnm4a9c2000u0a31sam2uxoo'
+            }
+        }
+    }
+}, '{ id title body published}').then((data) => {
+    console.log(data);
+
+    //query all users
+    return prisma.query.users(null, '{id, name, posts {id title}}');
+}).then((data) => {
+    console.log(JSON.stringify(data, undefined, 2));
+})
