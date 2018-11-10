8083ac3bc8bb5ba6512244592bb728f36270b3e2
sagar rathna sabapathy <sagar.sabapathy@gmx.de>
2018-11-10 20:33:53 +0100

    updatePost mutation using auth token

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -130,7 +130,20 @@ const Mutation = {
         }, info)
 
     },
-    async updatePost (parent, args, {prisma}, info) {
+    async updatePost (parent, args, {prisma, request}, info) {
+        const userId = getUserId(request);
+
+        //check if post exists for a user based on his auth token
+        const postExists = await prisma.exists.Post({
+            id: args.id,
+            author: {
+                id: userId
+            }
+        })
+
+        if (!postExists) {
+            throw new Error('Unable to delete post');
+        }
 
         return prisma.mutation.updatePost({
             data: args.data,
