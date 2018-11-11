db500b6488c3e6ad70a1d87bd07b7cac4c25bfb9
sagar rathna sabapathy <sagar.sabapathy@gmx.de>
2018-11-11 20:56:04 +0100

    email type field only for authenticated user

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,4 +1,19 @@
+import getUserId from '../utils/getUserId';
+
 const User = {
+    email (parent, args, ctx, info) {
+        const {request} = ctx;
+
+        const userId = getUserId(request, false);
+
+        //check if userId exists and the userId is equal to the parent.id
+        if (userId && userId === parent.id) {
+            return parent.email;
+        } else {
+            return null;
+        }
+
+    }
 
 };
 
