20ce5500b88fc4b90963758aa2f085d050c32831
sagar rathna sabapathy <sagar.sabapathy@gmx.de>
2018-11-11 12:14:35 +0100

    myPosts query using auth token

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -35,6 +35,29 @@ const Query = {
         return prisma.query.posts(opArgs, info);
 
     },
+    async myPosts (parent, args, {prisma, request}, info) {
+        //get the userId based on the auth token
+        const userId = getUserId(request);
+
+        const opArgs = {
+            where: {
+                author: {
+                    id: userId
+                }
+            }
+        };
+
+        if (args.query) {
+            opArgs.where.OR = [{
+                title_contains: args.query
+            }, {
+                body_contains: args.query
+            }]
+        }
+
+        return prisma.query.posts(opArgs, info);
+
+    },
     comments (parent, args, { prisma }, info) {
         //return db.comments;
 
