3ae139a97309a25f028879644aa62d5a1885163e
sagar rathna sabapathy <sagar.sabapathy@gmx.de>
2018-11-12 08:04:00 +0100

    fragment for User posts

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -2,6 +2,9 @@ import getUserId from '../utils/getUserId'
 
 const User = {
     email: {
+         //name of the fragment - userId
+        //from User
+        //the fields that we want - id
         fragment: 'fragment userId on User { id }',
         resolve(parent, args, { request }, info) {
             const userId = getUserId(request, false)
@@ -11,6 +14,22 @@ const User = {
             } else {
                 return null
             }
+        }
+    },
+    posts: {
+        fragment: 'fragment userId on User { id }',
+        resolve (parent, args, ctx, info) {
+            const { prisma } = ctx;
+
+            return prisma.query.posts({
+                where: {
+                    published: true,
+                    author: {
+                        id: parent.id
+                    }
+                }
+            }, info)
+
         }
     }
 }
